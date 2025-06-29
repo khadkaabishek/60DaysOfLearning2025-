ed49dcdeea562c2269e10c395480e93e65949169
khadkaabishek <abishekkhadka36@gmail.com>
2025-06-29 22:12:17 +0545

    regex for search

---
+++ Day20-29/E_commerce_App/Backend/middlewares/auth.js
@@ -0,0 +1,37 @@
+// middleware/auth.js
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+const JWT_SECRET = process.env.JWT_SECRET || "your_jwt_secret";
+
+exports.protect = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
+
+    if (!token) {
+      return res.status(401).json({ message: "Not logged in" });
+    }
+
+    const decoded = jwt.verify(token, JWT_SECRET);
+    const user = await User.findById(decoded.userId);
+
+    if (!user) {
+      return res.status(401).json({ message: "User no longer exists" });
+    }
+    req.user = user;
+
+    console.log(req.user);
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+};
+exports.restrictTo = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({
+        message: "You do not have permission for this action",
+      });
+    }
+    next();
+  };
+};
---
+++ Day20-29/E_commerce_App/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import "./footer.css";
+const Footer = () => (
+    <footer className="footer">
+      © {new Date().getFullYear()} E-Commerce App. All rights reserved.
+    </footer>
+  );
+  
+  export default Footer;
+  
