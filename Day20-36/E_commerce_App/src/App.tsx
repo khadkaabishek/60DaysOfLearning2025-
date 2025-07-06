2c92c7a37140e0c24be310190ff648e52bba6cc7
khadkaabishek <abishekkhadka36@gmail.com>
2025-07-06 22:49:30 +0545

    popup + delete item if it is yours

---
+++ Day20-36/E_commerce_App/src/App.tsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Auth/Login";
+import Signup from "./pages/Auth/Signup";
+import Dashboard from "./pages/home_dash/Dashboard";
+import Home from "./pages/home_dash/Home";
+import MainLayout from "./layouts/MainLayout";
+import AddItem from "./pages/Manipulate/AddItem";
+import SingleItem from "./pages/SingleItem";
+import Cart from "./pages/Cart";
+import EditItemInfo from "./pages/Manipulate/EditItemInfo";
+function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(!!localStorage.getItem("token"));
+
+  useEffect(() => {
+    const handleStorageChange = () => {
+      setIsAuthenticated(!!localStorage.getItem("token"));
+    };
+    window.addEventListener("storage", handleStorageChange);
+    return () => window.removeEventListener("storage", handleStorageChange);
+  }, []);
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainLayout><Home /></MainLayout>} />
+        <Route path="/login" element={<MainLayout><Login setIsAuthenticated={setIsAuthenticated} /></MainLayout>} />
+        <Route path="/signup" element={<MainLayout><Signup /></MainLayout>} />
+
+        <Route path="/add-item" element={isAuthenticated ? <MainLayout><AddItem /></MainLayout> : <Navigate to="/login" />} />
+        
+        <Route path="/dashboard" element={isAuthenticated ? <MainLayout><Dashboard /></MainLayout> : <Navigate to="/login" />} />
+        <Route path="/product/:id" element={<MainLayout><SingleItem/></MainLayout>} />
+         <Route path="/:id/edit_item_info" element = { <MainLayout> <EditItemInfo/></MainLayout> }></Route>
+        <Route path="/your_cart" element={<MainLayout><Cart/></MainLayout>} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ Day20-36/E_commerce_App/src/components/ItemCard.tsx
@@ -0,0 +1,92 @@
+import React from "react";
+import "./../styles/ItemCard.css";
+import { useNavigate } from "react-router-dom";
+const backendURL = "http://localhost:5001/";
+
+
+interface ItemCardProps {
+  prodData: {
+    _id : string,
+    name: string;
+    description: string;
+    price: number;
+    originalPrice?: number;
+    discountPercent?: number;
+    image?: string;
+  }[];
+}
+type Product = {
+  _id: string;
+  name: string;
+  description: string;
+  price: number;
+  originalPrice?: number;
+  discountPercent?: number;
+  quantityAvailable?: number;
+  image?: string;
+  colorOptions?: string[];
+};
+
+
+interface usertype {
+  name: string;
+  email: string;
+  role: string;
+}
+const ItemCard: React.FC<ItemCardProps> = ({ prodData }) => {
+
+  const handleEdit = (product: Product) => {
+    navigate(`/${product._id}/edit_item_info`, { state: { product } });
+  };
+  const navigate = useNavigate();
+  const storedUser = localStorage.getItem("user");
+  const user: usertype | null = storedUser ? JSON.parse(storedUser) : null;
+
+  const handleViewMore = (product: typeof prodData[0]) => {
+    navigate(`/product/${product._id}`, { state: { product } });
+  };
+
+  return (
+    <div className="card-row">
+      {prodData.map((item, index) => (
+        <div key={index} className="card">
+          {item.image && (
+            <img
+              src={`${backendURL}${item.image}`}
+              alt={item.name}
+              className="card-image"
+            />
+          )}
+          <div className="card-details">
+            <h3 className="card-title">{item.name}</h3>
+            <div className="card-price-section">
+              <span className="price">Rs.{item.price}</span>
+              {item.originalPrice && (
+                <>
+                  <span className="original-price">Rs.{item.originalPrice}</span>
+                  {item.discountPercent && (
+                    <span className="discount">-{item.discountPercent}%</span>
+                  )}
+                </>
+              )}
+            </div>
+            <div className="card-buttons">
+              {user?.role === "admin" ? (
+                <button className="btn buy-btn" onClick={()=>{handleEdit(item)}}>Edit Info</button>
+              ) : (
+                <button className="btn buy-btn">Buy Now</button>
+              )}
+              <button
+                onClick={() => handleViewMore(item)}
+                className="btn view-btn"
+              >
+                View More
+              </button>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+export default ItemCard;
