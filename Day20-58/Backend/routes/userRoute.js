7ab701f53955dc6516bc2dfc3420fd1917fa1bd7
khadkaabishek <abishekkhadka36@gmail.com>
2025-07-29 20:41:12 +0545

    day 58 too

--- Day20-58/Backend/routes/userRoute.js
+++
@@ -1,5 +0,0 @@
-const express = require("express");
-const { getNotifications } = require("../AdminController/getNotification");
-const router = express();
-router.get("/getNotification", getNotifications);
-module.exports = router;
