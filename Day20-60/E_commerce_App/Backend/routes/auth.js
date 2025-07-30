7220289e8a8c8556a180f5a35b8c0c06d72a36be
khadkaabishek <abishekkhadka36@gmail.com>
2025-07-30 23:00:55 +0545

    verified signup

---
+++ Day20-60/E_commerce_App/Backend/routes/auth.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express();
+const {
+  handleSignup,
+  verifyOTP,
+  handleCompleteSignup
+} = require("./../controllers/signup.js");
+const { handleLogin } = require("./../controllers/login.js");
+const { googleLogin } = require("../controllers/googlelogin.js");
+router.post("/signup", handleSignup);
+router.post("/verify-otp", verifyOTP);
+router.post("/complete-signup", handleCompleteSignup);
+router.post("/login", handleLogin);
+router.get("/google", googleLogin);
+module.exports = router;
